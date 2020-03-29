8e2d05faa1bb883d5b3d56cd1a658c62e6d84661
Milad <khaghanim2@hotmail.com>
2020-03-30 01:35:46 +0200

    add slug to views and set link in index directory

--- blog/views.py
+++ blog/views.py
@@ -1,14 +1,21 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
+from .models import Post
 
 
 # Create your views here.
 def index(request):
-    return HttpResponse("salam azizam. ")
+    poster = Post.objects.all()
+    return render(request, 'index.html', {'poster': poster})
 
 
-def post(request):
-    return HttpResponse("Bia baghalam balaa")
+def post(request, slug):
+    """
+
+    :type slug: object
+    """
+    print(slug)
+    return HttpResponse(slug)
--- mysite/urls.py
+++ mysite/urls.py
@@ -19,7 +19,7 @@ from blog import views as blog_views
 
 
 urlpatterns = [
-    url(r'^post/$', blog_views.post),
+    url(r'^post/(.*)$', blog_views.post),
     url(r'^$', blog_views.index),
     url(r'^admin/', admin.site.urls),
 ]
